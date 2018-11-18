073123a8119439d71e4d4dfc4bb9512aaa57dc2e
codychen <suigetuyamo@gmail.com>
2018-11-19 02:08:56 +0800

    first

---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/golang-collections/collections/stack"
+)
+
+func notOperator(s rune) bool {
+	return s >= 'a' && s <= 'z' || s >= 'A' && s <= 'Z' || s >= '0' && s <= '9'
+}
+
+func isOperator(s rune) bool {
+	return !(s >= 'a' && s <= 'z' || s >= 'A' && s <= 'Z' || s >= '0' && s <= '9')
+}
+
+func getPriority(s rune) int {
+	if s == '-' || s == '+' {
+		return 1
+	} else if s == '*' || s == '/' {
+		return 2
+	} else if s == '^' {
+		return 3
+	}
+
+	return 0
+}
+
+func infixToPostfix(infix string) string {
+	infix = "(" + infix + ")"
+
+	charStack := stack.New()
+	var output string
+
+	// (l-k/a)*(c/b-a)
+	for _, value := range infix {
+
+		if notOperator(value) {
+			output += string(value)
+		} else if value == '(' {
+			charStack.Push("(")
+		} else if value == ')' {
+			for {
+				if charStack.Peek().(string) != "(" {
+					output += charStack.Peek().(string)
+					charStack.Pop()
+				} else {
+					break
+				}
+			}
+
+			charStack.Pop()
+		} else {
+			if charStack.Peek() != nil {
+				if isOperator(rune(charStack.Peek().(string)[0])) {
+					for {
+						if getPriority(value) <= getPriority(rune(charStack.Peek().(string)[0])) {
+							output += charStack.Peek().(string)
+							charStack.Pop()
+						} else {
+							break
+						}
+					}
+
+					// Push current Operator on stack
+					charStack.Push(string(value))
+				}
+			}
+		}
+	}
+
+	return output
+}
+
+func infixToPrefix(infix string) string {
+	infix = reverseString(infix)
+	binfix := []byte(infix)
+	// Replace ( with ) and vice versa
+	for i := 0; i < len(infix); i++ {
+
+		if binfix[i] == '(' {
+			binfix[i] = ')'
+			i++
+		} else if binfix[i] == ')' {
+			binfix[i] = '('
+			i++
+		}
+	}
+	infix = string(binfix)
+
+	var prefix string = infixToPostfix(infix)
+
+	// Reverse postfix
+	prefix = reverseString(prefix)
+
+	return prefix
+}
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
+}
+
+func main() {
+	var s string = ("(a-b/c)*(a/k-l)")
+	output := infixToPrefix(s)
+
+	fmt.Println(output)
+}
